032269b82f3cadb8ce1824c6ee15b1a77022d0c3
henry0823 <henry1989123@gmail.com>
2017-01-11 01:40:46 +0800

    bmi

--- app/Http/Controllers/BmiController.php
+++ app/Http/Controllers/BmiController.php
@@ -3,63 +3,66 @@
 use Illuminate\Controller\Request;
 use App\Http\Controllers\Controller;
 
-
-$W = $_POST["weight"];
-$H = $_POST["height"];
-$S = $_POST["sex"];
-
-if($S == 'male')
-
+class PagesController extends Controller
 {
 
-	$bmi = $W/($H/100)/($H/100);
+	$W = $_POST["weight"];
+	$H = $_POST["height"];
+	$S = $_POST["sex"];
 
-	echo 'BMI：'.$bmi.'</br>';
+	if($S == 'male')
 
-	if($bmi >= '28')
 	{
-		echo "評比：肥豬";
+
+		$bmi = $W/($H/100)/($H/100);
+
+		echo 'BMI：'.$bmi.'</br>';
+
+		if($bmi >= '28')
+		{
+			echo "評比：肥豬";
+		}
+		elseif($bmi < '28' && $bmi >= '24')
+		{
+			echo "評比：正常";
+		}
+		elseif($bmi < '24' && $bmi >= '20')
+		{
+			echo "評比：偏瘦";
+		}
+		else
+		{
+			echo "評比：瘦皮猴";
+		}
+
 	}
-	elseif($bmi < '28' && $bmi >= '24')
-	{
-		echo "評比：正常";
-	}
-	elseif($bmi < '24' && $bmi >= '20')
-	{
-		echo "評比：偏瘦";
-	}
+
 	else
+
 	{
-		echo "評比：瘦皮猴";
+
+		$bmi = $W/($H/100)/($H/100);
+
+		echo 'BMI：'.$bmi.'</br>';
+
+		if($bmi >= '28')
+		{
+			echo "評比：肉肉Der";
+		}
+		elseif($bmi < '28' && $bmi >= '24')
+		{
+			echo "評比：還行";
+		}
+		elseif($bmi < '24' && $bmi >= '20')
+		{
+			echo "評比：太瘦";
+		}
+		else
+		{
+			echo "評比：皮包骨";
+		}
+
 	}
-
-}
-
-else
-
-{
-
-	$bmi = $W/($H/100)/($H/100);
-
-	echo 'BMI：'.$bmi.'</br>';
-
-	if($bmi >= '28')
-	{
-		echo "評比：肉肉Der";
-	}
-	elseif($bmi < '28' && $bmi >= '24')
-	{
-		echo "評比：還行";
-	}
-	elseif($bmi < '24' && $bmi >= '20')
-	{
-		echo "評比：太瘦";
-	}
-	else
-	{
-		echo "評比：皮包骨";
-	}
-
 }
 
 ?>
