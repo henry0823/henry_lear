4ef490743bcaa200a0058510e34ecd995ccdb4ba
henry0823 <henry1989123@gmail.com>
2017-01-11 02:22:35 +0800

    bmi

--- resources/views/pages/result.blade.php
+++ resources/views/pages/result.blade.php
@@ -1,68 +1,6 @@
 @extends('layout/style1')
 @section('Hello')
 
-<?php
-
-
-
-$W = $_POST['weight'];
-$H = $_POST['height'];
-$S = $_POST['sex'];
-
-if($S == 'male')
-
-{
-
-	$bmi = $W/($H/100)/($H/100);
-
-	echo 'BMI：'.$bmi.'</br>';
-
-	if($bmi >= '28')
-	{
-		echo "評比：肥豬";
-	}
-	elseif($bmi < '28' && $bmi >= '24')
-	{
-		echo "評比：正常";
-	}
-	elseif($bmi < '24' && $bmi >= '20')
-	{
-		echo "評比：偏瘦";
-	}
-	else
-	{
-		echo "評比：瘦皮猴";
-	}
-
-}
-
-else
-
-{
-
-	$bmi = $W/($H/100)/($H/100);
-
-	echo 'BMI：'.$bmi.'</br>';
-
-	if($bmi >= '28')
-	{
-		echo "評比：肉肉Der";
-	}
-	elseif($bmi < '28' && $bmi >= '24')
-	{
-		echo "評比：還行";
-	}
-	elseif($bmi < '24' && $bmi >= '20')
-	{
-		echo "評比：太瘦";
-	}
-	else
-	{
-		echo "評比：皮包骨";
-	}
-
-}
-
-?>
+123
 
 @stop
