2339550027be6fa522ef7b0d9758baa6e747594c
henry0823 <henry1989123@gmail.com>
2017-01-13 00:30:09 +0800

    bmi

--- resources/views/pages/result.blade.php
+++ resources/views/pages/result.blade.php
@@ -1,10 +1,18 @@
 @extends('layout/style1')
 @section('Hello')
 
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
 <?php
-
 return function result();
-
 ?>
+
+</body>
+</html>
 
 @stop
