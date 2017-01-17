4fa4af43345fa28ef570d666edbc04619461b6ab
henry0823 <henry1989123@gmail.com>
2017-01-18 02:22:36 +0800

    guess

--- resources/views/pages/guess.blade.php
+++ resources/views/pages/guess.blade.php
@@ -1,18 +1,7 @@
 @extends('layout/style1')
 @section('Hello')
 
-<?php
 
-$favorite = array('看電視','打電動','運動','逛街','打手槍');
-
-echo '猜猜小銘平常喜歡做的事：'.'<br>';
-
-foreach ($favorite as $key => $gun) 
-{
-	echo '$key'.'：'.'$gun'.'<br>';
-}
-
-?>
 
 <form action="/solution" method="get">
 請輸入：<input type="text" name="right" size="5"></br>
