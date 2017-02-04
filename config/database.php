f7b8f2ec7cff605c634f208ecf43cf0015892e6c
henry0823 <henry1989123@gmail.com>
2017-02-04 13:39:13 +0800

    database

--- config/database.php
+++ config/database.php
@@ -52,13 +52,13 @@ return [
             'prefix' => '',
         ],
 
-        'mysql' => [
+        'mysql' =>  [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'henry'),
+            'username' => env('DB_USERNAME', 'henry'),
+            'password' => env('DB_PASSWORD', 'Ab#12345'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
