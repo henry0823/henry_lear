1815550a616daed85ee52c1093cc1ba4f4e44785
henry0823 <henry1989123@gmail.com>
2017-02-07 17:34:10 +0800

    page

--- resources/views/layout/style1.blade.php
+++ resources/views/layout/style1.blade.php
@@ -56,6 +56,12 @@
 
         </div><!-- /.blog-main -->
 
+        <div class="col-sm-8 blog-main">
+
+        @yield('answer')
+
+        </div>
+
         <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
           <div class="sidebar-module sidebar-module-inset">
             <h4>About</h4>
--- resources/views/pages/solution.blade.php
+++ resources/views/pages/solution.blade.php
@@ -2,6 +2,27 @@
 
 @section('Hello')
 
+<?php
+
+$favorite = ['看電視','打電動','運動','逛街','打手槍'];
+
+echo '猜猜小銘平常喜歡做的事：'.'<br>';
+
+foreach ($favorite as $key => $gun) 
+{
+	echo $key.'：'.$gun.'<br>';
+}
+?>
+
+<form action="/solution" method="get">
+請輸入：<input type="text" name="right" size="5"></br>
+<input type="submit" value="確定">
+</form>
+
+@stop
+
+@section('answer')
+
 {{ $ture }}
 
 @stop
