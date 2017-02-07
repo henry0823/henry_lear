eb4f6f61c4594e14eab48efdfc080416dce90090
henry0823 <henry1989123@gmail.com>
2017-02-07 17:31:23 +0800

    page

--- resources/views/layout/style1.blade.php
+++ resources/views/layout/style1.blade.php
@@ -56,12 +56,6 @@
 
         </div><!-- /.blog-main -->
 
-        <div class="col-sm-8 blog-main">
-
-        @yield('answer')
-
-        </div>
-
         <div class="col-sm-3 col-sm-offset-1 blog-sidebar">
           <div class="sidebar-module sidebar-module-inset">
             <h4>About</h4>
