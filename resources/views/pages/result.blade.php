e99cd56fec4f4caa7d9ef4910798efa87f3967a1
henry0823 <henry1989123@gmail.com>
2017-02-07 17:28:29 +0800

    page

--- resources/views/pages/result.blade.php
+++ resources/views/pages/result.blade.php
@@ -1,4 +1,18 @@
 @extends('layout/style1')
+
+@section('content')
+
+<form action="/result" method="get">
+身高：<input type="text" name="height" size="7"></br>
+體重：<input type="text" name="weight" size="7"></br>
+性別：<input type="radio" name="sex" value="male">男
+<input type="radio" name="sex" value="female">女</br>
+<input type="submit" value="確定">
+<input type="reset" name="reset" value="重設">
+</form>
+
+@stop
+
 @section('answer')
 
 {{ $result }}<br>
