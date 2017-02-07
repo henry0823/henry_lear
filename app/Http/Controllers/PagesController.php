f186536d54a0967e2f4d1bb8a6d30a864fee1733
henry0823 <henry1989123@gmail.com>
2017-02-07 17:05:38 +0800

    same page

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -12,78 +12,88 @@ class PagesController extends Controller
 	}
 
 	public function guess()
+	
 	{
-		$R = $_GET['right'];
+		if($right != '')
+		{
+			return view('pages.guess');
+		}
+		elseif  
+		{
+			{
+				$R = $_GET['right'];
 
-		if($R == '打電動' or $R == '1')
-		{
-			$ture = '好聰明，答對了！';
-		}
-		elseif($R == '打手槍' or $R == '4')
-		{
-			$ture = '怎麼可能會是打手槍呢！';
-		}
-		else
-		{
-			$ture = '可惜，答錯了';
-		}
+				if($R == '打電動' or $R == '1')
+				{
+					$ture = '好聰明，答對了！';
+				}
+				elseif($R == '打手槍' or $R == '4')
+				{
+					$ture = '怎麼可能會是打手槍呢！';
+				}
+				else
+				{
+					$ture = '可惜，答錯了';
+				}
 
-		return view('pages.guess',compact('ture'));
+				return view('pages.guess',compact('ture'));
+			}
+		}
 	}
 
 
 	public function bmi()
 	{
 		
-		$W = $_GET['weight'];
-		$H = $_GET['height'];
-		$S = $_GET['sex'];
-		$value = $W/($H/100)/($H/100);
+			$W = $_GET['weight'];
+			$H = $_GET['height'];
+			$S = $_GET['sex'];
+			$value = $W/($H/100)/($H/100);
 
-		if($S == 'male')
+			if($S == 'male')
 
-		{
-			$result = 'BMI：'.$value;
+			{
+				$result = 'BMI：'.$value;
 
-			if($value >= '28')
-			{
-				$answer = "評比：肥豬";
+				if($value >= '28')
+				{
+					$answer = "評比：肥豬";
+				}
+				elseif($value < '28' && $value >= '24')
+				{
+					$answer = "評比：正常";
+				}
+				elseif($value < '24' && $value >= '20')
+				{
+					$answer = "評比：偏瘦";
+				}
+				else
+				{
+					$answer = "評比：瘦皮猴";
+				}
 			}
-			elseif($value < '28' && $value >= '24')
-			{
-				$answer = "評比：正常";
-			}
-			elseif($value < '24' && $value >= '20')
-			{
-				$answer = "評比：偏瘦";
-			}
+
 			else
 			{
-				$answer = "評比：瘦皮猴";
-			}
-		}
+				$result = 'BMI：'.$value;
 
-		else
-		{
-			$result = 'BMI：'.$value;
-
-			if($value >= '28')
-			{
-				$answer = "評比：肉肉Der";
+				if($value >= '28')
+				{
+					$answer = "評比：肉肉Der";
+				}
+				elseif($value < '28' && $value >= '24')
+				{
+					$answer = "評比：還行";
+				}
+				elseif($value < '24' && $value >= '20')
+				{
+					$answer = "評比：太瘦";
+				}
+				else
+				{
+					$answer = "評比：皮包骨";
+				}
 			}
-			elseif($value < '28' && $value >= '24')
-			{
-				$answer = "評比：還行";
-			}
-			elseif($value < '24' && $value >= '20')
-			{
-				$answer = "評比：太瘦";
-			}
-			else
-			{
-				$answer = "評比：皮包骨";
-			}
-		}
 	return view('pages.bmi',compact('result','answer'));
 	}
 }
